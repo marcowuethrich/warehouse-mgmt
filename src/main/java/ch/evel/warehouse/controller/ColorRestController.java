1f4ce40db5522e4266190468d20be67460ef7eeb
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2017-11-03 18:50:04 +0100

    remove function

--- src/main/java/ch/evel/warehouse/controller/ColorRestController.java
+++ src/main/java/ch/evel/warehouse/controller/ColorRestController.java
@@ -1,14 +1,20 @@
 package ch.evel.warehouse.controller;
 
+import ch.evel.warehouse.db.dao.ColorRepository;
 import ch.evel.warehouse.db.dao.ColorRestRepository;
 import ch.evel.warehouse.db.model.Color;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class ColorRestController {
@@ -16,9 +22,18 @@ public class ColorRestController {
     @Autowired
     private ColorRestRepository colorRestRepository;
 
+    @Autowired
+    private ColorRepository colorRepository;
+
     @JsonView(DataTablesOutput.View.class)
     @RequestMapping(path = "/data/colors", method = RequestMethod.GET)
     public DataTablesOutput<Color> getColors(DataTablesInput input) {
         return colorRestRepository.findAll(input);
+    }
+
+    @RequestMapping(value = "/data/color/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable("id") UUID uuid) {
+        colorRepository.delete(uuid);
+        return new ResponseEntity<Color>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/resources/static/js/datatables.js
+++ src/main/resources/static/js/datatables.js
@@ -1,12 +1,25 @@
 var colorTableEditor;
 $(document).ready(function () {
-var colorTable = $('table#colorTable');
+    var colorTable = $('table#colorTable');
 
     //Color Editor
-    colorTableEditor = new $.fn.dataTable.Editor( {
-        ajax: "data/colors",
+    colorTableEditor = new $.fn.dataTable.Editor({
+        ajax: {
+            create: {
+                type: 'PUT',
+                url: '/data/color/create'
+            },
+            edit: {
+                type: 'PUT',
+                url: '/data/color/edit/_id_'
+            },
+            remove: {
+                type: 'DELETE',
+                url: '/data/color/delete/_id_'
+            }
+        },
         table: colorTable,
-        idSrc:  'id',
+        idSrc: 'id',
         fields: [{
             label: "Code:",
             name: "code"
@@ -20,6 +33,8 @@ var colorTable = $('table#colorTable');
     colorTable.on('click', 'tbody td:not(:first-child)', function (e) {
         colorTableEditor.bubble(this);
     });
+    //Disable row inline editing
+    colorTable.off('click', 'tbody td:not(:first-child)');
 
     //Color DataTable
     colorTable.DataTable({
@@ -42,7 +57,7 @@ var colorTable = $('table#colorTable');
         }],
         order: [[1, "asc"]],
         select: {
-            style:    'os',
+            style: 'os',
             selector: 'td:first-child'
         },
         buttons: [
