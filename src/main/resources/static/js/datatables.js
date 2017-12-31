0a27f5d2cb516f850e13a0bb720c7c63080a6abc
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2017-12-31 19:40:15 +0100

    Remove color modal with selected item

--- src/main/resources/static/js/datatables.js
+++ src/main/resources/static/js/datatables.js
@@ -1,8 +1,8 @@
+var colorTable = $('table#colorTable');
+
 $(document).ready(function () {
-    var colorTable = $('table#colorTable');
-
-
-    colorTable.DataTable({
+    //Init
+    colorTable = colorTable.DataTable({
         processing: true,
         serverSide: true,
         ajax: {
@@ -24,8 +24,35 @@ $(document).ready(function () {
         "order": [[ 2, "asc" ]]
     });
 
+    //Select
     $('#colorTable').find('tbody').on( 'click', 'tr', function () {
         $(this).toggleClass('selected');
     });
 
+
 });
+
+//Get Selected Items
+function loadSelectedItem() {
+    var array = [];
+    colorTable.rows('.selected').every(function(rowIdx) {
+        array.push(colorTable.row(rowIdx).data())
+    });
+
+    //Clear Remove Item List
+    $('#addColorModal').find('ul').empty();
+
+    if(array.length < 1) {
+        $('p#emptyText').removeClass('hidden');
+        $('p#removeText').addClass('hidden');
+        $('#addColorModal button').last().addClass('hidden');
+    }else {
+        $('p#removeText').removeClass('hidden');
+        $('p#emptyText').addClass('hidden');
+        $('#addColorModal button').last().removeClass('hidden');
+
+        array.forEach(function (item) {
+            $('#addColorModal').find('ul').append('<li class="list-group-item"><a>' + item.name + '</a></li>')
+        })
+    }
+}
