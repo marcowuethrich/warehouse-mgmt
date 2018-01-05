ebdf1b4b3ceb59f64945abb38c93239808ac6e43
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2018-01-05 20:34:24 +0100

    create element

--- src/main/java/ch/evel/warehouse/db/dao/TypGroupRepository.java
+++ src/main/java/ch/evel/warehouse/db/dao/TypGroupRepository.java
@@ -1,11 +1,14 @@
 package ch.evel.warehouse.db.dao;
 
 import ch.evel.warehouse.db.model.TypGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface TypGroupRepository extends JpaRepository<TypGroup, UUID> {
+public interface TypGroupRepository extends DataTablesRepository<TypGroup, UUID> {
     boolean existsByName(String name);
     boolean existsByCode(String code);
+
+    List<TypGroup> findAll();
 }
