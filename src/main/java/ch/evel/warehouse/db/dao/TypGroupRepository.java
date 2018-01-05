d39e9f7dea7884991d5129f5ec15f84823dedb5d
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2018-01-05 20:48:17 +0100

    sort typGroup dropdown

--- src/main/java/ch/evel/warehouse/db/dao/TypGroupRepository.java
+++ src/main/java/ch/evel/warehouse/db/dao/TypGroupRepository.java
@@ -1,14 +1,14 @@
 package ch.evel.warehouse.db.dao;
 
 import ch.evel.warehouse.db.model.TypGroup;
-import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface TypGroupRepository extends DataTablesRepository<TypGroup, UUID> {
+public interface TypGroupRepository extends CrudRepository<TypGroup, UUID> {
     boolean existsByName(String name);
     boolean existsByCode(String code);
 
-    List<TypGroup> findAll();
+    List<TypGroup> findAllByOrderByCodeAsc();
 }
