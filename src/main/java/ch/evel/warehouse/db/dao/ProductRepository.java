30a778683b7e663e88e6c5414b399fde3215f3a4
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2018-01-08 20:24:43 +0100

    add/edit product

--- src/main/java/ch/evel/warehouse/db/dao/ProductRepository.java
+++ src/main/java/ch/evel/warehouse/db/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package ch.evel.warehouse.db.dao;
 
+import ch.evel.warehouse.db.model.Article;
+import ch.evel.warehouse.db.model.Location;
 import ch.evel.warehouse.db.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface ProductRepository extends CrudRepository<Product, UUID> {
+    boolean existsProductByArticle(Article article);
+
+    boolean existsProductByLocation(Location location);
 }
