a63ac8e9df2626af7925271e7654d81d40b6b306
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2018-01-09 22:16:29 +0100

    dropdown location in table

--- src/main/resources/static/js/datatable/datatable-productLocationEdit.js
+++ src/main/resources/static/js/datatable/datatable-productLocationEdit.js
@@ -14,8 +14,9 @@ $(document).ready(function () {
             {"data": "amount"},
             {
                 render: function (data, type, row, meta) {
-                    return '<form action="/admin/productLocationEdits/create">' +
-                        '<a>Test Loc.</a>';
+                    return '<select id=\"locationProductEditLocationSelectInTable' + meta.row + '\" class=\"form-control\"' +
+                        '                            onchange=\"changeFieldColor(' + row + ')\"' +
+                        '                            title=\"Location\"></select>';
                 }
             },
             {
@@ -51,8 +52,8 @@ function filterProductEditTable() {
         table.column(2).search(locationName).draw();
     } else {
         table.column(2).search("").draw();
-
     }
+    initDropdown();
 }
 
 
@@ -62,4 +63,20 @@ function loadTableChangeToModal() {
 
 function submitProductLocationTableChange() {
 
+}
+
+function changeFieldColor(row) {
+
+}
+
+function initDropdown() {
+    var table = $('table#productLocationEditTable').DataTable();
+    table.rows().every(function (rowIdx, tableLoop, rowLoop) {
+        var select = $('#locationProductEditLocationSelectInTable' + rowLoop);
+        select.empty();
+        $('#locationProductEditLocationSelect').find('option').clone().appendTo('#locationProductEditLocationSelectInTable' + rowLoop);
+        select.find('option:selected').removeAttr('selected');
+        select.children().first().remove();
+        select.append('<option value="nothing" selected="selected"> - </option>');
+    });
 }
