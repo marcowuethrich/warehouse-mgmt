5806354f79574524ab8c1fd6131b02f194cf70d5
Marco Wüthrich <marco.wuethrich.pr@gmail.com>
2018-01-10 13:35:19 +0100

    amount input field in table with validation

--- src/main/resources/static/js/datatable/datatable-productLocationEdit.js
+++ src/main/resources/static/js/datatable/datatable-productLocationEdit.js
@@ -15,14 +15,14 @@ $(document).ready(function () {
             {
                 render: function (data, type, row, meta) {
                     return '<select id=\"locationProductEditLocationSelectInTable' + meta.row + '\" class=\"form-control\"' +
-                        '                            onchange=\"changeFieldColor(this)\"' +
+                        '                            onchange=\"changeLocationFieldColor(this)\"' +
                         '                            title=\"Location\"></select>';
                 }
             },
             {
                 render: function (data, type, row, meta) {
-                    return '<form action="/admin/productLocationEdits/create">' +
-                        '<a>an other test Amount</a>';
+                    return '<input type="number" id="locationProductEditAmountSelectInTable" ' +
+                        'onchange="changeAmountFieldColor(this)" class="form-control" placeholder="1" \>';
                 }
             }
         ],
@@ -64,7 +64,7 @@ function submitProductLocationTableChange() {
 
 }
 
-function changeFieldColor(select) {
+function changeLocationFieldColor(select) {
     var tr = $(select).closest('tr');
     var or_loc = tr.find('td:eq(1)').text();
     var ed_loc = $(select).find('option:selected').text();
@@ -72,6 +72,25 @@ function changeFieldColor(select) {
         $(select).closest('td').removeAttr("style");
     } else {
         $(select).closest('td').css('background', '#ffc10745');
+    }
+}
+
+function changeAmountFieldColor(input) {
+    var tr = $(input).closest('tr');
+    var or_amount = Number(tr.find('td:eq(2)').text());
+    var ed_amount = Number($(input).val());
+
+    if (or_amount < ed_amount && ed_amount % 1 === 0) {
+        $(input).val(or_amount);
+    } else if (ed_amount < 0 || ed_amount % 1 !== 0) {
+        $(input).val(0);
+    }
+
+
+    if (ed_amount === 0 || ed_amount < 0 || ed_amount % 1 !== 0) {
+        $(input).closest('td').removeAttr("style");
+    } else {
+        $(input).closest('td').css('background', '#ffc10745');
     }
 }
 
